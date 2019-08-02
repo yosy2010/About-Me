b031020547eb3d7b6e5da11ef529fe5b449d01ba
YASSER ALTAMIMI <yosy2010@gmail.com>
2019-08-02 13:30:04 +0300

    removed data binding and just used kotlin extension

--- app/src/main/java/com/tamimi/aboutme/MainActivity.kt
+++ app/src/main/java/com/tamimi/aboutme/MainActivity.kt
@@ -4,15 +4,18 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputBinding
 import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
 
+
+
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         doneBtnView.setOnClickListener {
             addNickName(it)
 
