52bafbe5ee1a5a698839f4a95c7b39c058af4338
Yasser Tamimi <yosy2010@gmail.com>
2019-08-02 21:02:25 +0300

    fixed the error

--- app/src/main/java/com/tamimi/aboutme/MainActivity.kt
+++ app/src/main/java/com/tamimi/aboutme/MainActivity.kt
@@ -13,7 +13,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     // this is the binding object for main activity
+    // here we init the binding object and giving it a class of type(ActivityMainBinding) this class name is generated for us by using the name of the activity xml file and change it to camel case and add Binding at the end
     private lateinit var binding : ActivityMainBinding
+
+    // here we init the data variable that we will be using to give data to the ui
     private val myName : MyName = MyName("YASSER ALTAMIMI")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +24,8 @@ class MainActivity : AppCompatActivity() {
         // here we're setting the layout using the data binding util
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // connect the data in my data class to the binding object
+        // connect the name data in my data class to the binding object
+        // here we are giving it the whole object we created above so that we can extract information from it again
         binding.myName = myName
 
         doneBtnView.setOnClickListener {
@@ -30,17 +34,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNickName(view: View?) {
-        myName?.nickname = nameEditTxtView.text.toString()
 
-        // invalidate all binding expressions and requests a nre rebind to refresh
-        invalidateAll()
-
+        // here we are taking the text entered in the edit text and assigning it to the object nickName
+        binding.myName?.nickname = nameEditTxtView.text.toString()
+        nickNameTxtView.visibility = View.VISIBLE
         nameEditTxtView.visibility = View.GONE
-        enterdNmeTxtView.visibility = View.VISIBLE
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view?.windowToken, 0)
+
+        // invalidate all binding expressions and requests a nre rebind to refresh
+        binding.invalidateAll()
     }
 
 }
